55f9e024c9aa24c620c10a1bf44c735a1a683aa1
Matthew Knight <mattknight.sa@gmail.com>
2020-01-08 12:21:53 -0600

    update app id

--- server.js
+++ server.js
@@ -33,30 +33,41 @@ app.get("/api/notes", function(req, res) {
   })
 })
 
-app.get("/api/notes/:id", function(req, res) {
-  var chosen = req.params.id;
-  console.log(chosen)
-  // fs.readFileSync('./db.json', 'utf8', function(err, data) {
-  //   data = JSON.parse(data);
-  //  res.end(JSON.stringify(data));
-  //  console.log(data);
-  // })
+// app.get("/api/notes/:id", function(req, res) {
+//   var chosen = req.params.id;
+//   var removeNote;
+//   chosen = parseInt(chosen);
+  
+//   console.log(notes[1].id);
+//   // console.log(typeof chosen);
+//   // notes = notes.splice(chosen,chosen);
+//   for(i = 0; i < notes.length; i++) {
+//    if(notes[i].id === chosen){
+//     notes = notes.splice(i,i);
+//     var data = JSON.stringify(notes, null, 2);
+//     fs.writeFileSync('db.json', data, (err) => {
+//       if (err) throw err;
+//       console.log('data written to file')
+//       console.log(data)
+//     });
+//    }
+//   }
 
-})
+//   })
 
 app.post("/api/notes", function(req, res) {
    newNote = req.body;
    newNote.id = num;
-
-  // console.log(newNote);
+// console.log(newNote);
   notes.push(newNote);
   console.log(notes)
   var data = JSON.stringify(notes, null, 2);
-  fs.writeFileSync('db.json', data, (err) => {
+  fs.writeFile('db.json', data, (err) => {
     if (err) throw err;
     console.log('data written to file')
   });
 num++;
+// return notes;
 })
 
 app.listen(PORT, function() {
